f164424f2ee7487ca595eecaec833d409dc0b91c
sujeethcodes <sujeethcodes@gmail.com>
2025-07-01 16:32:06 +0530

    fibonacci_series-output-format-change

--- problems/fibonacci_series.go
+++ problems/fibonacci_series.go
@@ -4,9 +4,10 @@ import "fmt"
 
 func Fibonacci() {
 	a, b := 0, 1
+	val := ""
 	for i := 0; i <= 10; i++ {
-		fmt.Println(a, " ")
+		val += fmt.Sprint(a, " ")
 		a, b = a+b, a
-
 	}
+	fmt.Println(val)
 }
